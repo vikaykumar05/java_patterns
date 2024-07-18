8bfd645f6087de76d7f711dcf0b879c7cbad36c8
vikaykumar05 <93151564+vikaykumar05@users.noreply.github.com>
2024-07-19 04:47:52 +0530

    visit on 19/07/24

---
+++ src/Numbers/pattern1.java
@@ -0,0 +1,87 @@
+package Numbers;
+import java.util.*;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//--------------------11111111-------------------
+		/*
+		int n=6;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print("*");
+			}
+			System.out.println("   ");
+		}
+		output:
+			******   
+			******   
+			******   
+			******   
+			******   
+			****** 
+			
+			*/
+//------------------------222222----------------------	
+		/*
+		int n=6;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println( );
+		}
+		output :
+			*
+			**
+			***
+			****
+			*****
+			******
+			
+			*/
+
+//		----------------333333-----------------
+		/*
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println( );
+		}
+		output:
+		1
+		12
+		123
+		1234
+		12345
+		*/
+		
+		
+		
+
+//-------------------444444444444-------------
+		/*
+		int n=5;
+		for(int i=1;i<=n;i++) {                      
+			for(int j=1;j<=i;j++) {    
+				System.out.print(i);
+			}
+			System.out.println( );
+		}
+		output 
+		1
+		22
+		333
+		4444
+		55555
+		*/
+
+//-----------------55555-----------
+		
+		
+	}
+
+}
